9ab82c07b577c285edabaa4d940ec265e830414e
bposey <mcpdude@gmail.com>
2024-03-01 14:40:57 -0700

    okay tests without an assert aren't great

--- tests/test_config.py
+++ tests/test_config.py
@@ -9,3 +9,5 @@ from src.csv_cleaner.config import load_json_config
 
 def test_load_config():
 	config = load_json_config('tests/test_data/test_data_1_config.json')
+	assert 'input' in config.keys()
+	assert 'output' in config.keys()
