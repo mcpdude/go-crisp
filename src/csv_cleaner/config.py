70effa4462bd3f860a2d171232c61f905b727820
bposey <mcpdude@gmail.com>
2024-03-01 15:47:10 -0700

    now we're verifying the config

--- src/csv_cleaner/config.py
+++ src/csv_cleaner/config.py
@@ -6,3 +6,26 @@ def load_json_config(path):
 		raw_config = json.load(json_file)
 
 	return raw_config
+
+def verify_config(config: dict):
+	# This checks that the config has a valid schema
+	assert 'input' in config.keys()
+	assert 'output' in config.keys()
+	
+	# input tests
+	assert 'expected_columns' in config['input'].keys()
+	assert len(config['input']['expected_columns']) > 0
+	for column in config['input']['expected_columns']:
+		assert 'column_name' in column.keys()
+		assert 'regex' in column.keys()
+
+	# output tests
+	assert 'columns' in config['output'].keys()
+	assert len(config['output']['columns']) > 0
+	for column in config['output']['columns']:
+		assert 'name' in column.keys()
+		assert 'type' in column.keys()
+		assert 'nullable' in column.keys()
+		assert 'transforms' in column.keys()
+
+	return config
--- tests/test_config.py
+++ tests/test_config.py
@@ -5,9 +5,15 @@ import os
 _parentdir = pathlib.Path(__file__).parent.parent.resolve()
 sys.path.insert(0, str(_parentdir))
 
-from src.csv_cleaner.config import load_json_config
+from src.csv_cleaner.config import load_json_config, verify_config
 
 def test_load_config():
 	config = load_json_config('tests/test_data/test_data_1_config.json')
 	assert 'input' in config.keys()
 	assert 'output' in config.keys()
+
+# TODO: would be good to use a fixture here to avoid loading twice
+def test_verify_config():
+	config = load_json_config('tests/test_data/test_data_1_config.json')
+	verified_config = verify_config(config)
+	assert type(verified_config) == dict
