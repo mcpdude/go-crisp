13b6e7dcd33c84a1c1cd384b3a813ff04ff5b4ee
bposey <mcpdude@gmail.com>
2024-03-01 14:23:09 -0700

    cool now we can write csv's as well

--- src/csv_cleaner/csv.py
+++ src/csv_cleaner/csv.py
@@ -15,10 +15,12 @@ def write_csv(path, rows: dict, existing=False):
     # wrapper around the CSV library for writing results. Use existing to append to files already written
     if existing:
         with open(path, 'a') as csv_file:
+            writer = csv.DictWriter(csv_file)
             for row in rows:
                 writer.writerow(row)
     else:
         with open(path, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, rows[0].keys())
             writer.writeheader()
             for row in rows:
                 writer.writerow(row)
--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,15 +1,29 @@
 import pathlib
 import sys
+import os
 
 _parentdir = pathlib.Path(__file__).parent.parent.resolve()
 sys.path.insert(0, str(_parentdir))
 
-from src.csv_cleaner.csv import load_csv
+from src.csv_cleaner.csv import load_csv, write_csv
 
 def test_load_csv():
 	data = load_csv('tests/test_data/test_data_1.csv')
 	# breakpoint()
 	assert (len(data)) == 2
 
-def write_csv():
-	pass
+def test_write_csv():
+	# remove test files, if any
+	try:
+		os.remove('tests/test_data/test_data_1_output.csv')
+	except FileNotFoundError:
+		# if there's a failure with this test, or if it's never been run before, 
+		# you're not going to have a file to remove
+		pass
+	except Exception as e:
+		raise e
+
+	data = load_csv('tests/test_data/test_data_1.csv')
+	write_csv('tests/test_data/test_data_1_output.csv', data)
+	written_data = load_csv('tests/test_data/test_data_1_output.csv')
+	assert data == written_data
