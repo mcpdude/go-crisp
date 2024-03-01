68fb1b7ea2c2bd364dd3d4bb69a3f7ef1e6cc94a
bposey <mcpdude@gmail.com>
2024-03-01 14:40:06 -0700

    and you gotta have some tests too.

---
+++ tests/test_config.py
@@ -0,0 +1,11 @@
+import pathlib
+import sys
+import os
+
+_parentdir = pathlib.Path(__file__).parent.parent.resolve()
+sys.path.insert(0, str(_parentdir))
+
+from src.csv_cleaner.config import load_json_config
+
+def test_load_config():
+	config = load_json_config('tests/test_data/test_data_1_config.json')
