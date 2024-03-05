f5e234a72a3dc5721e8800a8ddbcb3c297320046
bposey <mcpdude@gmail.com>
2024-03-05 14:57:40 -0700

    we're making the rows nowwq

--- src/csv_cleaner/transform.py
+++ src/csv_cleaner/transform.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from decimal import Decimal
 
 def cast_to_int(input_value):
 	""" This function takes a single input value and casts it to integer"""
@@ -13,6 +14,12 @@ def cast_to_str(input_value):
 
 	return str(input_value)
 
+def cast_to_decimal(input_value):
+	""" This function takes a single input value and casts it to decimal"""
+	input_value = input_value.replace(',', '')
+	return Decimal(input_value)
+
+
 def casing(input_value, casing):
 	""" This function takes a single input value and converts it to the correct casing"""
 	if casing == 'proper':
@@ -26,7 +33,51 @@ def casing(input_value, casing):
 	return output_value
 
 def concat(input_values, concat_value=''):
+	""" This function takes input values and concatenates them with a optional concat value"""
 	return concat_value.join(input_values)
 
 def constant(constant_value):
+	""" This function returns a constant value given. Yes, it is superfluous."""
 	return constant_value
+
+def do_the_transforms(row, transforms):
+	for transform in transforms:
+		if transform['type'] == 'cast_int':
+			row[transform['output_column_name']] = cast_to_int(
+					row[transform['input_column']])
+
+		elif transform['type'] == 'cast_datetime':
+			row[transform['output_column_name']] = cast_to_datetime(
+					row[transform['input_column']], transform['datetime_format'])
+
+		elif transform['type'] == 'cast_str':
+			row[transform['output_column_name']] = cast_to_str(
+					row[transform['input_column']])
+
+		elif transform['type'] == 'cast_decimal':
+			row[transform['output_column_name']] = cast_to_decimal(
+					row[transform['input_column']])
+
+		elif transform['type'] == 'casing':
+			row[transform['output_column_name']] = casing(
+					row[transform['input_column']], transform['casing_choice'])
+
+		elif transform['type'] == 'concat':
+			# don't like this
+			input_values = [row[key] for key in transform['input_columns']]
+
+			if transform['concat_value']:
+				row[transform['output_column_name']] = concat(
+					input_values, transform['concat_value'])
+			else:
+				row[transform['output_column_name']] = concat(
+					input_values)
+
+		elif transform['type'] == 'constant':
+			# try:
+			row[transform['output_column_name']] = constant(
+					transform['constant_value'])
+		else:
+			print(f"Transform of type: {transform['type']} not recognized.")
+
+	return row
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -7,7 +7,7 @@ sys.path.insert(0, str(_parentdir))
 
 from src.csv_cleaner.csv import load_csv, write_csv
 from src.csv_cleaner.config import load_json_config, verify_config
-from src.csv_cleaner.transform import cast_to_int, cast_to_datetime, cast_to_str, concat, constant, casing
+from src.csv_cleaner.transform import do_the_transforms
 
 def test_e2e():
 	# load config
@@ -17,48 +17,14 @@ def test_e2e():
 	# load CSV
 	data = load_csv('tests/test_data/test_data_1.csv')
 
-
-
-	# apply transforms
-	# verify input columns 
 	bad_rows = []
 	good_rows = []
 	for row in data:
 		for input_column in verified_config['input']:
 			pass
 		# create transformed data
-		for transform in verified_config['transforms']:
-			if transform['type'] == 'cast_int':
-				row[transform['output_column_name']] = cast_to_int(
-						row[transform['input_column']])
-
-			elif transform['type'] == 'cast_datetime':
-				row[transform['output_column_name']] = cast_to_datetime(
-						row[transform['input_column']], transform['datetime_format'])
-
-			elif transform['type'] == 'cast_str':
-				row[transform['output_column_name']] = cast_to_str(
-						row[transform['input_column']])
-
-			elif transform['type'] == 'casing':
-				row[transform['output_column_name']] = casing(
-						row[transform['input_column']], transform['casing_choice'])
-
-			elif transform['type'] == 'concat':
-				# don't like this
-				input_values = [row[key] for key in transform['input_columns']]
-
-				if transform['concat_value']:
-					row[transform['output_column_name']] = concat(
-						input_values, transform['concat_value'])
-				else:
-					row[transform['output_column_name']] = concat(
-						input_values)
-
-			elif transform['type'] == 'constant':
-				# try:
-				row[transform['output_column_name']] = constant(
-						transform['constant_value'])
+		
+		row = do_the_transforms(row, verified_config['transforms'])
 
 		# select down to requested rows
 		output_row = {}
